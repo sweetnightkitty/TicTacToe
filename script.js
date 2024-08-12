ed9243a9465f42047fd9770a5c7f939893881ab1
SaraToth <saratoth101@gmail.com>
2024-08-13 08:46:53 +0900

    Create getBoard function that generates 2d array for game play in console

---
+++ script.js
@@ -0,0 +1,20 @@
+//function getBoard - to create the board
+function getBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for(let i = 0; i < rows; i++) {
+        board[i] = [];
+        for(let j = 0; j < columns; j++) {
+            board[i].push(0);
+        }
+    }
+
+    return board;
+}
+
+
+//funtion gameController - controls game play behind the scenes
+
+//function screenController - controls the UI and screen
